a84634c381a1fa2d983a9c8d38433e0bee690c46
User <User@LAPTOP-R84IC4C7>
2018-02-01 17:47:40 +0800

    init

---
+++ src/controller/Main.java
@@ -0,0 +1,41 @@
+package controller;
+
+import model.*;
+import view.*;
+
+import java.io.File;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/Sample.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("/view/application.css").toExternalForm());
+
+			// Set icon and title
+			primaryStage.getIcons().add(new Image("file:image/chicken.png"));
+			primaryStage.setTitle("PKU Raman Spectrum");
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
