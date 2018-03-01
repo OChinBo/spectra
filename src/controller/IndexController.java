cf5cff9c3031da7db6110fb0b2b50d64af6ed78a
User <User@192.168.31.1>
2018-03-01 11:17:12 +0800

    0301

--- src/controller/IndexController.java
+++ src/controller/IndexController.java
@@ -1,6 +1,12 @@
 package controller;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 import com.sun.glass.ui.Window;
 
@@ -19,38 +25,74 @@ public class IndexController {
 
 	private File sourceFile;
 
-	//@FXML
-	//private javafx.scene.control.MenuItem closeButton;
-
+	// @FXML
+	// private javafx.scene.control.MenuItem closeButton;
 
 	@FXML
-	private void closeWindowsAction(){
-		//System.exit(0);
-	    Platform.exit();
+	private void closeWindowsAction() {
+		// System.exit(0);
+		Platform.exit();
 	}
-
 
 	/***** MenuBar Buttons *****/
 	@FXML
-	private void openFileAction(ActionEvent ae){
+	private void openFileAction(ActionEvent ae) {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open File");
 
-		sourceFile = fileChooser.showOpenDialog(null); // Assign selected file to local parameter.
-		//System.out.print(sourceFile);
+		// Assign selected file to local parameter
+		sourceFile = fileChooser.showOpenDialog(null);
+
+		// System.out.print(sourceFile);
+		readFileByLines(sourceFile);
+
 	}
 
 	@FXML
-	private void saveAsAction(ActionEvent ae){
+	private void saveAsAction(ActionEvent ae) {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Save As");
 
-		FileChooser.ExtensionFilter csvFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv"); // Set format of filter.
+		// Set format of filter
+		FileChooser.ExtensionFilter csvFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
 		FileChooser.ExtensionFilter txtFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
 		fileChooser.getExtensionFilters().add(csvFilter);
 		fileChooser.getExtensionFilters().add(txtFilter);
 
 		sourceFile = fileChooser.showSaveDialog(null);
-		//System.out.print(sourceFile);
+
+		// System.out.print(sourceFile);
+	}
+
+	private void readFileByLines(File file) {
+		//Not Completed
+		try{
+			BufferedReader  in = new BufferedReader(new FileReader(file));
+			//Scanner in = new Scanner(new FileReader(file));
+
+			// 一次读入一行，直到读入null为文件结束
+			int line = 1;
+			String tempString = null;
+
+			//跳過第一行文檔標頭
+			System.out.println(in.readLine());
+
+			while ((tempString = in.readLine()) != null) {
+				// 显示行号
+				System.out.println("line " + line + ": " + tempString);
+				line++;
+			}
+
+			in.close();
+
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+
+
+	}
+
+	private void showChart(){
+		// Not Completed
 	}
 }
