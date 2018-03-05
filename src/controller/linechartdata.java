53c0cf40e1a0bad82805a037062e6cac5a013ef2
discipleofhamilton <discipleofhamilton@users.noreply.github.com>
2018-03-05 16:53:38 +0800

    linechart的資料結構

---
+++ src/controller/linechartdata.java
@@ -0,0 +1,34 @@
+package controller;
+
+public class linechartdata {
+
+	private Double x ;
+	private Double y ;
+	
+	linechartdata() {
+		this.x = 0.0 ;
+		this.y = 0.0 ;
+	} // end of CONSTRUCTOR
+	
+	public void setX( Double x ) {
+		this.x = x ;
+	} // end of setX
+	
+	public void setY( Double y ) {
+		this.y = y ;
+	} // end of setY
+	
+	public void setXY( Double x, Double y) {
+		this.x = x ;
+		this.y = y ;
+	} // end of setXY
+	
+	public Double getX() {
+		return this.x ;
+	} // end of getXY
+	
+	public Double getY() {
+		return this.y ;
+	} // end of getY
+	
+}
