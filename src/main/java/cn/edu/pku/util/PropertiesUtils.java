8a6e41c5c317d5da89c718d9635785aa214c2c02
User <User@192.168.31.1>
2018-04-10 14:19:06 +0800

    改下import的方式

--- src/main/java/cn/edu/pku/util/PropertiesUtils.java
+++ src/main/java/cn/edu/pku/util/PropertiesUtils.java
@@ -1,7 +1,11 @@
 package cn.edu.pku.util;
 
 import java.io.InputStream;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 public class PropertiesUtils {
 
