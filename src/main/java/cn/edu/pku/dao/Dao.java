a104c03c364c1497351f1cce22fe95044b21be7f
Hamilton Chang <discipleofhamilton@users.noreply.github.com>
2018-04-19 09:24:19 +0800

    0419 update Dao.java

--- src/main/java/cn/edu/pku/dao/Dao.java
+++ src/main/java/cn/edu/pku/dao/Dao.java
@@ -1,6 +1,6 @@
 package cn.edu.pku.dao;
 
-public abstract class Dao<T,D> {
+public abstract class Dao<T,D,L> {
 	// Basic Dao<> Class
 	// T is read() method's return type
 	// D is write() method's input type
@@ -8,4 +8,7 @@ public abstract class Dao<T,D> {
 	public abstract T read();
 
 	public abstract void write(D data);
+
+	public abstract void write(D data, L lc);
+	//123
 }
